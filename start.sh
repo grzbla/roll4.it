4f2058217070092b62f5be72ac90b7cd8a363385
grzbla <47889281+grzbla@users.noreply.github.com>
2021-12-24 05:54:31 +0100

    css tweaks, fixed paths

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CONFIG_PATH="/mnt/f/warsztat/dnd5/penandpaper/config/nginx/nginx.conf"
+CONFIG_PATH="/mnt/f/warsztat/dnd5/roll4.it/config/nginx/nginx.conf"
 /usr/sbin/nginx -c $CONFIG_PATH
