30374495c4b30e7626240b25e01d148715651372
grzbla <47889281+grzbla@users.noreply.github.com>
2023-02-17 15:06:34 +0100

    removed metro, started writing tile system

--- static/js/system.js
+++ static/js/system.js
@@ -17,9 +17,15 @@ import browserDetect from "../lib/browser-detect.es5.js"
     }
 }
 
-function PotatoRPG()
+function get(selector)
+{
+    return document.querySelector(selector)
+}
+
+function RPG()
 {
     this.fileDelay = 100
+
     /*
         substitute disallowedChars found in string with replacement
     */
@@ -38,63 +44,64 @@ function PotatoRPG()
     */
     this.sanitize = (string) =>
     {
-        string = this.replacer(string, this.handlers.dnd.disallowedChars.remove, "")
-        string = this.replacer(string, this.handlers.dnd.disallowedChars.underscore, "_")
+        string = this.replacer(string, this.ui.dnd.disallowedChars.remove, "")
+        string = this.replacer(string, this.ui.dnd.disallowedChars.underscore, "_")
         return string
     }
 
-    //this is obsolete i think
-    this.createDirectory = (p) =>
+    this.user =
     {
-        fs.createDirectory(p).then((result) =>
-        {
-            console.log(p)
-            const info =
-            {
-                name: p.split("/").pop(),
-                stamp: Date.now()
-            }
 
-            console.log(info)
-            fs.writeFile(p + "/info.json", info).then((nfo) =>
-            {
-                console.log(nfo)
-            })
-        }).catch((exception) =>
-        {
-            const error = new Error();
-            console.log("%c" + exception.message, style.color1b)
-            console.log("%c" + error.stack, style.color1b)
-
-            // capture directory assertion error
-            const match = (/\"(.*?)\" directory does not exist/g).exec(exception.message)
-            if (match && match[1])
-            {
-                console.log("%c" + match[1], style.color1a)
-                setTimeout((path) =>
-                {
-                    if (path.slice(-1) == "/")
-                        path.pop()
-
-                    this.createDirectory(path).then(() =>
-                    {
-                        fs.exists(path).then((result) =>
-                        {
-                            result ? console.log("%c" + path) : this.createDirectory(p)
-                        })
-                    })
-                }, this.fileDelay, match[1])
-            }
-            //TODO finish when it occurs again
-        })
     }
 
-    /*
-        handler functions
-    */
-    this.handlers =
+    this.network =
     {
-        // drag and drop
+
+    }
+
+    this.ui =
+    {
+        gridWidth: 10,
+        gridHeight: 6,
+        click: (event) =>
+        {
+            const rect = event.target.getBoundingClientRect()
+            const x = event.clientX - rect.left, y = event.clientY - rect.top
+            const height = event.target.offsetHeight, width = event.target.offsetWidth
+
+            console.log(x, width, y, height)
+            console.log(width - x, width * 0.5)
+            const right = (width - x) < (width * 0.5)
+            const top = (height - y) > (height * 0.5)
+
+            //todo select closest side
+            let effect;
+            if (right)
+            {
+                effect = "click-effect-right"
+            }
+            else if (top)
+            {
+                effect = "click-effect-top"
+            }
+            else if (!right && !top)
+            {
+                effect = "click-effect-left"
+            }
+            else
+            {
+                effect = "click-effect-bottom"
+            }
+            event.target.classList.add(effect)
+            setTimeout((target) =>
+            {
+                target.classList.remove(effect)
+            }, 100, event.target)
+
+
+
+        },
+        // drag and drop1
         dnd:
         {
             //disallowed file name char list
@@ -105,14 +112,14 @@ function PotatoRPG()
             },
             drag: (event) => // prevent default on drag
             {
-                event.preventDefault();
+                event.preventDefault()
             },
             drop: (event) => //get files on drop
             {
-                event.preventDefault();
-                this.handlers.dnd.getFiles(event)
+                event.preventDefault()
+                this.ui.dnd.getFiles(event)
             },
-            saveFile: (item, path) => //saves item as blob to path
+            saveFile: (item, path, event) => //saves item as blob to path
             {
                 // Get file
                 item.file((file) =>
@@ -124,36 +131,61 @@ function PotatoRPG()
 
                         //create file object
                         const f = { name: this.sanitize(file.name), extension: ((splat.length > 1) ? splat.pop() : null), data: blob, type: file.type != "" ? file.type : null, size: file.size }
-                        this.handlers.dnd.writeFile(f, path)
+                        this.ui.dnd.writeFile(f, path, event)
                     });
                 });
             },
-            writeFile: async (f, path) => // writes f to path
+            writeFile: async (file, path, event) => // writes f to path
             {
+                //indexeddb-fs allows only limited char set
+                //TODO this sucks, change indexeddb-fs to something else
                 const pathSanitized = this.sanitize(path)
-                path = pathSanitized.slice(0, -1)
+                path = pathSanitized.slice(0, -1) + "/" + file.name
 
-                setTimeout((path, file) =>
-                {
-                    fs.writeFile(path, file).then((result) =>
-                    {
-                        console.log("%c" + path, style.color3a)
-                        //if path is currently displayed or home
-                        //add icons to current view
-                        this.fileDelayTick++
-                    })
-                }, this.fileDelay * this.fileDelayTick , pathSanitized + f.name, f)
+                await fs.writeFile(path, file)
+
+                //produce strings
+                let dir = path.split("/")
+                let filename = dir.pop()
+                const statsPath = dir.join("/") + "/stats.json"
+
+                //delay each file write a little bit
+
+                //stats file for faster file size calculation
+                let stats = {};
+                if (await fs.exists(statsPath)) //if stats.json exists, read it
+                    stats = await fs.readFile(statsPath)
+
+                if (!stats[file.name])
+                    stats[file.name] = {}
+
+                file.size = (file.size * 0.0009765625 * 0.0009765625).toFixed(3)
+                stats[file.name].size = file.size
+                await fs.writeFile(statsPath, stats)
+
+                console.log("%c" + path + ", " + (file.size) + "MiB", style.color3a)
+
 
             },
             readDroppedFiles: async (item, path, dropPath) => // reads files at current path recursively
             {
                 this.fileDelayTick = 1
                 path = path || "";
+
                 // iterate directory if directory
                 if (item.isDirectory)
                 {
                     const name = this.sanitize(item.name)
                     const p = dropPath + "/" + path + name
+
+                    if (dropPath == "home")
+                    {
+
+                    }
+                    //if path is currently displayed or home
+                    //add icons to current view
+                    // if ()
+
 
                     if (!(await fs.exists(p)))
                         await fs.createDirectory(p)
@@ -163,18 +195,18 @@ function PotatoRPG()
                     {
                         for (var i = 0; i < entries.length; i++)
                         {
-                            this.handlers.dnd.readDroppedFiles(entries[i], path + name + "/", dropPath)
+                            this.ui.dnd.readDroppedFiles(entries[i], path + name + "/", dropPath)
                         }
                     })
                 }
                 else if (item.isFile)//save file if file
                 {
-                    this.handlers.dnd.saveFile(item, dropPath + "/" + path)
+                    this.ui.dnd.saveFile(item, dropPath + "/" + path)
                 }
             },
             getFiles: (event) => // gets files from data transfer
             {
-                const dropPath = event.target.getAttribute("data-fs-path")
+                const dropPath = event.target.getAttribute("system-path")
 
                 const items = event.dataTransfer.items;
                 for (var i = 0; i < items.length; i++)
@@ -182,21 +214,12 @@ function PotatoRPG()
                     var item = items[i].webkitGetAsEntry();
                     if (item)
                     {
-                        this.handlers.dnd.readDroppedFiles(item, null, dropPath)
+                        this.ui.dnd.readDroppedFiles(item, null, dropPath, event)
                     }
                 }
             }
 
         },
-    }
-    this.user =
-    {
-
-    }
-
-    this.network =
-    {
-
     }
 
     /*
@@ -251,8 +274,25 @@ function PotatoRPG()
 
             //iterate home directory directories and files
             //for each dir/file put tile under stored coordinates
-            console.log(await fs.readDirectory("root"))
-            console.log(await fs.readDirectory("home"))
+            const home = await fs.readDirectory("home")
+            console.log(home.directories)
+            home.directories.forEach((directory) =>
+            {
+                console.log(directory)
+
+                let tile = document.createElement("div")
+                tile.className = "tile color1"
+                tile.setAttribute("onclick", "window.rpg.ui.click(event)")
+
+                let bar = document.createElement("div")
+                bar.className = "bar"
+                bar.textContent = directory.name
+
+                tile.appendChild(bar)
+                get("#screen").appendChild(tile)
+                //create directory tile
+                //style tile based on something
+            })
         }
     }
 
@@ -262,4 +302,4 @@ function PotatoRPG()
     this.init.begin()
 }
 
-window.rpg = new PotatoRPG()
+window.rpg = new RPG()
