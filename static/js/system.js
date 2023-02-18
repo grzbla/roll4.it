9bf5162a4874e2857ca2509376f15d0a7ff98984
grzbla <47889281+grzbla@users.noreply.github.com>
2023-02-18 08:03:54 +0100

    drag n drop complete

--- static/js/system.js
+++ static/js/system.js
@@ -61,8 +61,28 @@ function RPG()
 
     this.ui =
     {
-        gridWidth: 10,
-        gridHeight: 6,
+        gridWidth: 20,
+        gridHeight: 12,
+        tileSize:
+        {
+            mini: {x: 80, y: 80},
+            base: {x: 160, y: 160},
+            wide: {x: 320, y: 160},
+            tall: {x: 160, y: 320},
+            large: {x: 320, y: 320},
+            wider: {x: 640, y: 320},
+            taller: {x: 320, y: 640},
+            larger: {x: 640, y: 640}
+        },
+        getCursorPosition: (event) =>
+        {
+            const element = event.target
+            console.log(element)
+            var rect = element.getBoundingClientRect()
+            var x = event.clientX - rect.left
+            var y = event.clientY - rect.top
+            this.ui.cursor = [x, y]
+        },
         click: (event) =>
         {
             const rect = event.target.getBoundingClientRect()
@@ -73,6 +93,8 @@ function RPG()
             console.log(width - x, width * 0.5)
             const right = (width - x) < (width * 0.5)
             const top = (height - y) > (height * 0.5)
+
+            // cosnt r =
 
             //todo select closest side
             let effect;
@@ -101,6 +123,65 @@ function RPG()
 
 
         },
+        loadHome: async () =>
+        {
+            /*
+                HOME SCREEN INITIALIZATION
+            */
+            if(!(await fs.exists(path.home)))
+            {
+                console.log("%cHome directory needs to be created.", style.color2a)
+                let home = await fs.createDirectory(path.home)
+            }
+
+            const dir = await fs.readDirectory(path.home)
+            console.log("%cHome directory: " + JSON.stringify(dir, null, 2), style.color2c)
+
+            //iterate home directory directories and files
+            //for each dir/file put tile under stored coordinates
+            const home = await fs.readDirectory("home")
+            console.log(home.directories)
+            home.directories.forEach((directory) =>
+            {
+                this.ui.createTile(directory)
+            })
+        },
+        createTile: (item, event) =>
+        {
+            if (event)
+                console.log(event)
+
+            let tile = document.createElement("div")
+            tile.className = (item.tile && item.tile.size ? item.tile.size : "base") + "-tile color1"
+            tile.setAttribute("onclick", "window.rpg.ui.click(event)")
+            tile.setAttribute("onmousedown", "window.rpg.ui.dnd.mouseDown(event)")
+            tile.setAttribute("ondragend", "window.rpg.ui.dnd.dragEnd(event)")
+            tile.setAttribute("draggable", "true")
+
+            let bar = document.createElement("div")
+            bar.className = "bar"
+            bar.textContent = item.name
+
+            tile.appendChild(bar)
+            get("#home").appendChild(tile)
+            //create directory tile
+            //style tile based on something
+        },
+        subMinFromMax: (a, b) =>
+        {
+            return b > a ? b - a : a - b
+        },
+        snap: (position, size, offset) =>
+        {
+            //snap to grid
+            position.x = (Math.floor(position.x / size.x) * (size.x + 26))
+            position.y = (Math.floor(position.y / size.y) * (size.y + 26))
+             //adjust for padding
+            position.x += offset.x
+            position.y += offset.y
+
+            return position
+        },
         // drag and drop1
         dnd:
         {
@@ -114,10 +195,52 @@ function RPG()
             {
                 event.preventDefault()
             },
+            dragEnd: (event) =>
+            {
+                //assign position to dragged element
+                var element = event.srcElement
+                var rect = element.getBoundingClientRect()
+                var x = event.clientX
+                var y = event.clientY
+
+                if (!this.ui.cursor)
+                    this.ui.getCursorPosition(event)
+
+                element.classList.add("placed")
+            	let position = {x: x - this.ui.cursor[0], y: y - this.ui.cursor[1]}
+
+
+                const tileSizeName = element.getAttribute("class").match(/(.*?)-tile/)[1]
+                position = this.ui.snap(position, this.ui.tileSize[tileSizeName], {x: 50, y: 50})
+
+                element.style.setProperty("left", position.x + "px")
+                element.style.setProperty("top", position.y + "px")
+            },
             drop: (event) => //get files on drop
             {
                 event.preventDefault()
-                this.ui.dnd.getFiles(event)
+                if (event.dataTransfer.items.length > 0)
+                    this.ui.dnd.getFiles(event)
+                else
+                    console.log(event)
+            },
+
+            //mouse is clicked
+            mouseDown: (event) =>
+            {
+                this.ui.getCursorPosition(event)
+            },
+            raycast: (point, targets) =>
+            {
+                let output = []
+                for (let i = 0, l = targets.length; i < l; i++)
+                {
+                    const target =  array[i]
+                    let distance = {};
+
+                    output.push({x: this.ui.subMinFromMax(point.x, target.x), y: this.ui.subMinFromMax(point.y, target.y)})
+                }
+                return output;
             },
             saveFile: (item, path, event) => //saves item as blob to path
             {
@@ -130,7 +253,7 @@ function RPG()
                         const splat = file.name.split(".")
 
                         //create file object
-                        const f = { name: this.sanitize(file.name), extension: ((splat.length > 1) ? splat.pop() : null), data: blob, type: file.type != "" ? file.type : null, size: file.size }
+                        const f = { name: this.sanitize(file.name), extension: ((splat.length > 1) ? splat.pop() : null), blob: blob, type: file.type != "" ? file.type : null, size: file.size }
                         this.ui.dnd.writeFile(f, path, event)
                     });
                 });
@@ -158,7 +281,7 @@ function RPG()
 
                 if (!stats[file.name])
                     stats[file.name] = {}
-
+                // 0.0009765625 is 1/1024th
                 file.size = (file.size * 0.0009765625 * 0.0009765625).toFixed(3)
                 stats[file.name].size = file.size
                 await fs.writeFile(statsPath, stats)
@@ -167,7 +290,7 @@ function RPG()
 
 
             },
-            readDroppedFiles: async (item, path, dropPath) => // reads files at current path recursively
+            readDroppedFiles: async (item, path, dropPath, event) => // reads files at current path recursively
             {
                 this.fileDelayTick = 1
                 path = path || "";
@@ -178,14 +301,14 @@ function RPG()
                     const name = this.sanitize(item.name)
                     const p = dropPath + "/" + path + name
 
-                    if (dropPath == "home")
+                    if (path.split("/").length == 1) //if path one segment long
                     {
+                        //reload #home
+                        const home = get("#home")
+                        const string = home.innerHTML
 
+                        this.ui.createTile(item, event)
                     }
-                    //if path is currently displayed or home
-                    //add icons to current view
-                    // if ()
-
 
                     if (!(await fs.exists(p)))
                         await fs.createDirectory(p)
@@ -195,7 +318,7 @@ function RPG()
                     {
                         for (var i = 0; i < entries.length; i++)
                         {
-                            this.ui.dnd.readDroppedFiles(entries[i], path + name + "/", dropPath)
+                            this.ui.dnd.readDroppedFiles(entries[i], path + name + "/", dropPath, event)
                         }
                     })
                 }
@@ -259,40 +382,7 @@ function RPG()
             }
             console.log("%cInstance ID: " + JSON.stringify(this.instance, null, 2), style.color2b)
 
-
-            /*
-                HOME SCREEN INITIALIZATION
-            */
-            if(!(await fs.exists(path.home)))
-            {
-                console.log("%cHome directory needs to be created.", style.color2a)
-                let home = await fs.createDirectory(path.home)
-            }
-
-            const dir = await fs.readDirectory(path.home)
-            console.log("%cHome directory: " + JSON.stringify(dir, null, 2), style.color2c)
-
-            //iterate home directory directories and files
-            //for each dir/file put tile under stored coordinates
-            const home = await fs.readDirectory("home")
-            console.log(home.directories)
-            home.directories.forEach((directory) =>
-            {
-                console.log(directory)
-
-                let tile = document.createElement("div")
-                tile.className = "tile color1"
-                tile.setAttribute("onclick", "window.rpg.ui.click(event)")
-
-                let bar = document.createElement("div")
-                bar.className = "bar"
-                bar.textContent = directory.name
-
-                tile.appendChild(bar)
-                get("#screen").appendChild(tile)
-                //create directory tile
-                //style tile based on something
-            })
+            this.ui.loadHome(path)
         }
     }
 
